296252d03a0ad5cb299e15e65e8635f5abff1bb0
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-09 10:35:25 -0500

    creacion de la app con React

---
+++ src/components/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="margin">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Email
+            </th>
+            <th>
+              Link
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              Juan
+            </td>
+            <td>
+              juanjosevegaq@gmail.com
+            </td>
+            <td>
+              Juan.com
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Ana
+            </td>
+            <td>
+              ana@gmail.com
+            </td>
+            <td>
+              Ana.com
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default App;
