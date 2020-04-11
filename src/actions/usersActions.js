17b28605c48a1c3d6c89bedf1a5c9fd5fd7d781f
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-11 17:07:54 -0500

    Try Catch

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 import { BRING_USERS} from '../types/usersTypes'
 
 export const bringEverything = () => async (dispatch) => {
-  const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-  dispatch({
-    type: 'BRING_USERS',
-    payload: response.data
-  })
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+    dispatch({
+      type: 'BRING_USERS',
+      payload: response.data
+    })
+  } catch (error) {
+    console.log('Error: ', error.message)
+  }
 }
