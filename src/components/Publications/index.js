89c1ed19181e2eb487cb226fbacc59b5e12c6ed0
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-12 08:23:38 -0500

    compartir reducer

--- src/components/Publications/index.js
+++ src/components/Publications/index.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import * as usersActions from '../../actions/usersActions'
 
 class Publications extends Component {
+  componentDidMount() {
+    if (!this.props.users.length) {
+      this.props.bringEverything()
+    }
+  }
   render() {
     return (
       <div>
+        <h1>
+          Publications of
+        </h1>
         { this.props.match.params.key }
       </div>
     );
   }
 }
 
-export default Publications;
+const mapStateToProps = (reducers) => {
+  return reducers.usersReducer
+}
+
+export default connect(mapStateToProps, usersActions)(Publications);
