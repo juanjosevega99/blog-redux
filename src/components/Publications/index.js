d6152718ea37f3865789dfe84b8301d44d938202
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-04-12 22:29:42 -0500

    Evitar sobrescritura

--- src/components/Publications/index.js
+++ src/components/Publications/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import Spinner from '../General/Spinner';
+import Fatal from '../General/Fatal';
 
 import * as usersActions from '../../actions/usersActions'
 import * as publicationsActions from '../../actions/publicationsActions'
@@ -9,19 +11,52 @@ const { bringForUser: publicationsBringForUser } = publicationsActions
 
 class Publications extends Component {
   async componentDidMount() {
+    const {
+      usersBringEverything,
+      publicationsBringForUser,
+      match: { params: { key } }
+    } = this.props
+
     if (!this.props.usersReducer.users.length) {
-      await this.props.usersBringEverything()
+      await usersBringEverything()
     }
-    this.props.publicationsBringForUser(this.props.match.params.key)
+    if (this.props.usersReducer.error) {
+      return;
+    }
+    if (!('publications_key' in this.props.usersReducer.users[key])) {
+      publicationsBringForUser(key)
+    }
   }
+
+  addUser = () => {
+    const {
+      usersReducer,
+      match: { params: { key } }
+    } = this.props;
+
+    if (usersReducer.error) {
+      return <Fatal message={ usersReducer.error } />
+    }
+
+    if (!usersReducer.users.length || usersReducer.loading) {
+      return <Spinner />
+    }
+
+    const name = usersReducer.users[key].name;
+    
+    return (
+      <h1>
+        Publications of { name }
+      </h1>
+    )
+  }
+
   render() {
     console.log(this.props)
     return (
       <div>
-        <h1>
-          Publications of
-        </h1>
         { this.props.match.params.key }
+        { this.addUser() }
       </div>
     );
   }
